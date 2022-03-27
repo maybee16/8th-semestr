ca37fe3e6ca595d9d6dfab93e2e606f5ff17fab3
maybee16 <mayaochir@gmail.com>
2022-03-27 21:55:30 +0900

    .

---
+++ Data protection/ZaIn_Lab5_test1/ZaIn_Lab5_test1/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ZaIn_Lab5_test1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_MESSAGE = new System.Windows.Forms.TextBox();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.textBox_ENCRYPT = new System.Windows.Forms.TextBox();
+            this.button_Decrypt = new System.Windows.Forms.Button();
+            this.textBox_Decrypt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_MESSAGE
+            // 
+            this.textBox_MESSAGE.Location = new System.Drawing.Point(15, 77);
+            this.textBox_MESSAGE.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_MESSAGE.Name = "textBox_MESSAGE";
+            this.textBox_MESSAGE.Size = new System.Drawing.Size(500, 22);
+            this.textBox_MESSAGE.TabIndex = 0;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(15, 106);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(500, 23);
+            this.buttonEncrypt.TabIndex = 1;
+            this.buttonEncrypt.Text = "Зашифровать";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // textBox_ENCRYPT
+            // 
+            this.textBox_ENCRYPT.Location = new System.Drawing.Point(15, 188);
+            this.textBox_ENCRYPT.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_ENCRYPT.Name = "textBox_ENCRYPT";
+            this.textBox_ENCRYPT.Size = new System.Drawing.Size(500, 22);
+            this.textBox_ENCRYPT.TabIndex = 2;
+            // 
+            // button_Decrypt
+            // 
+            this.button_Decrypt.Location = new System.Drawing.Point(15, 217);
+            this.button_Decrypt.Name = "button_Decrypt";
+            this.button_Decrypt.Size = new System.Drawing.Size(500, 23);
+            this.button_Decrypt.TabIndex = 3;
+            this.button_Decrypt.Text = "Расшифровать";
+            this.button_Decrypt.UseVisualStyleBackColor = true;
+            this.button_Decrypt.Click += new System.EventHandler(this.button_Decrypt_Click);
+            // 
+            // textBox_Decrypt
+            // 
+            this.textBox_Decrypt.Location = new System.Drawing.Point(15, 297);
+            this.textBox_Decrypt.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox_Decrypt.Name = "textBox_Decrypt";
+            this.textBox_Decrypt.Size = new System.Drawing.Size(500, 22);
+            this.textBox_Decrypt.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Сообщение для зашифровки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Зашифрованное сообщение";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Расшифрованное сообщение";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(161, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "ПОТОКОВЫЙ ШИФР";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 350);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_Decrypt);
+            this.Controls.Add(this.button_Decrypt);
+            this.Controls.Add(this.textBox_ENCRYPT);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.textBox_MESSAGE);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_MESSAGE;
+        private System.Windows.Forms.Button buttonEncrypt;
+		private System.Windows.Forms.TextBox textBox_ENCRYPT;
+		private System.Windows.Forms.Button button_Decrypt;
+		private System.Windows.Forms.TextBox textBox_Decrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Data protection/ZaIn_Lab5_test1/ZaIn_Lab5_test1/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZaIn_Lab5_test1
+{
+    public partial class Form1 : Form
+    {
+        int[] KEY = new int[256]; //Ключ
+        int[] GEN = new int[256]; //Генератор случайных чисел
+
+        string MESSAGE_str = "";  //Сообщение, передаваемое по каналу связи
+        int[] MESSAGE_int_Unicode;//Сообщение в кодах символов Юникода
+        int[] MESSAGE_int_Bytes;  //Сообщение в байтах
+
+        int[] GAMMA;              //Гамма
+        int[] ENCRYPT;            //Зашифрованное сообщение в байтах
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 256; i++)
+            {
+                KEY[i] = i;
+                GEN[i] = i;
+            }
+
+            //1. Создание ключа. Ключ KEY представляет собой перестановку массива целых чисел от 0 до 255
+            Shuffle(KEY); // случайный ключ
+            for(int i = 0;i < 256; i++)
+            {
+               GEN[i] = KEY[i];
+            }
+
+
+            //2. Инициализация генератора случайных чисел. 
+            //Генератор случайных чисел GEN представляет собой массив целых чисел от 0 до 255. 
+            //В качестве исходного его состояния необходимо взять случайный ключ, полученный в пункте (1). 
+            //Далее состояние генератора инициализируется, то есть преобразуется по следующему правилу:
+            int j = 0;
+            int tmp = 0;
+            for(int i = 0; i < 256; i++)
+            {
+                j = (j + GEN[i] + KEY[i]) % 256;
+
+                tmp = GEN[i];
+                GEN[i] = GEN[j];
+                GEN[j] = tmp;
+            }
+        }
+
+        //Зашифровать сообщение
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            MESSAGE_str = textBox_MESSAGE.Text;
+            textBox_ENCRYPT.Text = "";
+
+            MESSAGE_int_Unicode = new int[MESSAGE_str.Length]; //коды Юникода символов сообщения
+            MESSAGE_int_Bytes = new int[MESSAGE_str.Length * 2]; //пары байтов сообщения
+
+            //Получение кодов Юникода символов сообщения
+            for (int i = 0; i < MESSAGE_int_Unicode.Length; i++)
+            {
+                MESSAGE_int_Unicode[i] = (int)MESSAGE_str[i];
+            }
+
+            //Получение пары байтов сообщения
+            int j = 0;
+            for (int i = 0; i < MESSAGE_str.Length; i++)
+            {
+                string tmpBit = Convert.ToString(MESSAGE_str[i], 2); //перевести сообщение в последовательность битов
+                if (tmpBit.Length < 8) //если на 1 символ - 1 байт (8 бит)
+                {
+                    tmpBit = CompleteZeroes(tmpBit, 8); //дописать вначале нули, чтобы было 8 бит, если не хватает
+
+                    for (j = j; j < MESSAGE_int_Bytes.Length; j++)
+                    {
+                        MESSAGE_int_Bytes[j] = 0; //1й байт всегда нулевой, т.к. на 1 символ - 1 байт
+                        j++;
+                        MESSAGE_int_Bytes[j] = Convert.ToInt32(BinToDec(tmpBit)); //перевести 2й байт из двоичного вида в десятичный
+                        j++;
+                        break;
+                    }
+                }
+                else //если на 1 символ - 2 байта
+                {
+                    tmpBit = CompleteZeroes(tmpBit, 16); //дописать вначале нули, чтобы было 16 бит (2 байта), если не хватает
+                    string tmpBit_p1 = "";
+                    string tmpBit_p2 = "";
+
+                    for(int p1 = 0; p1 < tmpBit.Length / 2; p1++) //получить первый байт в двоичном виде
+                    {
+                        tmpBit_p1 += tmpBit[p1];
+                    }
+                    for(int p2 = tmpBit.Length / 2; p2 < tmpBit.Length; p2++) //получить второй байт в двоичном виде
+                    {
+                        tmpBit_p2 += tmpBit[p2];
+                    }
+                    for (j = j; j < MESSAGE_int_Bytes.Length; j++)
+                    {
+                        MESSAGE_int_Bytes[j] = Convert.ToInt32(BinToDec(tmpBit_p1)); //перевести 1й байт из двоичного вида в десятичный
+                        j++;
+                        MESSAGE_int_Bytes[j] = Convert.ToInt32(BinToDec(tmpBit_p2)); //перевести 2й байт из двоичного вида в десятичный
+                        j++;
+                        break;
+                    }
+                }
+            }
+
+            //3. Получение гаммы. 
+            //Сообщение MESSAGE, которое передаёт по каналу связи отправитель, представляет собой байтовую последовательность. 
+            //Количество состояний одного байта – 256.
+            //Гамма GAMMA представляет собой массив чисел, взятых из диапазона от 0 до 255(соответствуют возможным состояниям байтов сообщения).
+            //Длина гаммы равна длине сообщения(в байтах). Чтобы построить гамму, необходимо выполнить следующие действия:
+            int i1 = 0;
+            int j1 = 0;
+            GAMMA = new int[MESSAGE_int_Bytes.Length];
+
+            //k – индекс буквы сообщения; перебрать все k в количестве, равном длине сообщения(в байтах),
+            //и на каждой итерации выполняя операции:
+            for (int k = 0; k < MESSAGE_int_Bytes.Length; k++) 
+            {
+                int tmp = 0;
+                int t = 0;
+
+                i1 = (i1 + 1) % 256;
+                j1 = (j1 + GEN[i1]) % 256;
+
+                //поменять местами GEN[i] и GEN[j];
+                tmp = GEN[i1];
+                GEN[i1] = GEN[j1];
+                GEN[j1] = tmp;
+
+                t = (GEN[i1] + GEN[j]) % 256;
+                GAMMA[k] = GEN[t];
+            }
+
+            //4. Шифрование сообщения. 
+            //К каждому байту сообщения применить операцию XOR c соответствующим байтом гаммы.
+            ENCRYPT = new int[MESSAGE_int_Bytes.Length];
+            for (int i = 0; i < MESSAGE_int_Bytes.Length; i++)
+            {
+                ENCRYPT[i] = MESSAGE_int_Bytes[i] ^ GAMMA[i];
+            }
+
+            //
+            for (int i = 0; i < ENCRYPT.Length; i++)
+			{
+                string EncryptBin = "";
+                char EncryptChar = '0';
+
+                EncryptBin += DecToBin(ENCRYPT[i].ToString()); //перевести 1й байт в двоичный код
+                i++;
+
+                EncryptBin += DecToBin(ENCRYPT[i].ToString()); //перевести 2й байт в двоичный код и сложить с первым   
+
+                EncryptChar = (char)Convert.ToInt32(EncryptBin, 2); //перевести полученный двоичный код в символ
+                textBox_ENCRYPT.Text += EncryptChar;
+            }
+        }
+
+        //Расшифровать сообщение
+		private void button_Decrypt_Click(object sender, EventArgs e)
+		{
+            textBox_Decrypt.Text = "";
+
+            //5. Расшифрование сообщения. 
+            //К каждому байту сообщения применить операцию XOR c соответствующим байтом гаммы.
+            int[] DECRYPT = new int[ENCRYPT.Length];
+            for (int i = 0; i < ENCRYPT.Length; i++)
+            {
+                DECRYPT[i] = ENCRYPT[i] ^ GAMMA[i];
+            }
+
+            for (int i = 0; i < DECRYPT.Length; i++)
+            {
+                string DecryptBin = "";
+                char DecryptChar = '0';
+
+                DecryptBin += DecToBin(DECRYPT[i].ToString()); //перевести 1й байт в двоичный код
+                i++;
+
+                //ДЛЯ АНГЛИЙСКИХ БУКВ (1 байт на символ)
+                if (DecryptBin == "0") //если 1й байт оказался 0
+                {
+                    DecryptBin += DecToBin(DECRYPT[i].ToString()); //перевести 2й байт в двоичный код и сложить с первым
+
+                    DecryptChar = (char)Convert.ToInt32(DecryptBin, 2); //перевести полученный двоичный код в символ
+                    textBox_Decrypt.Text += DecryptChar;
+                }
+                //ДЛЯ РУССКИХ БУКВ (2 байта на символ)
+                else
+                {
+                    string DecryptBin_p1 = DecryptBin; //первый байт в двоичном коде
+                    DecryptBin_p1 = CompleteZeroes(DecryptBin_p1, 8); //дописать вначале нули, чтобы стало 8 символов
+
+                    string DecryptBin_p2 = DecToBin(DECRYPT[i].ToString()); //перевести 2й байт в двоичный код
+                    DecryptBin_p2 = CompleteZeroes(DecryptBin_p2, 8); //дописать вначале нули, чтобы стало 8 символов
+
+                    DecryptBin = DecryptBin_p1 + DecryptBin_p2; //сложить 1й и 2й байты в двоичном виде
+
+                    DecryptChar = (char)Convert.ToInt32(DecryptBin, 2); //перевести полученный двоичный код в символ
+                    textBox_Decrypt.Text += DecryptChar;
+                }
+            }
+        }
+
+
+        //Случайно перемешать список из чисел
+        public static Random rng = new Random((int)DateTime.Now.Ticks & 0x0000FFFF);
+        public static void Shuffle(int[] list)
+        {
+            int n = list.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                int value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        //Перевести двоичную строку в десятичную
+        public string BinToDec(string bin)
+        {
+            return Convert.ToInt32(bin, 2).ToString();
+        }
+
+        //Перевести десятичную строку в двоичную
+        public string DecToBin(string bin)
+        {
+            return Convert.ToString(Int32.Parse(bin), 2);
+        }
+
+        //Дополнить двоичное число нулями, в соответствии с разрядом
+        string CompleteZeroes(string bin, int R)
+        {
+            if (bin.Length != R)
+            {
+                int dif = R - bin.Length;
+                string difZeroes = "";
+
+                for (int i = 0; i < dif; i++)
+                {
+                    difZeroes += "0";
+                }
+
+                return difZeroes + bin;
+            }
+            else
+                return bin;
+        }
+    }
+}
